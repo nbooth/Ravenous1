3d8e16d562a6cbbdb4d3cd044c998e7c6d7f562e
Natalie Booth <nbdragongirl@Natalies-MacBook-Air-2.local>
2017-12-04 17:22:48 -0800

    Current state of ravenous filepart 1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+//import React from 'react';
+import './App.css';
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import BusinessList from './components/BusinessList/BusinessList';
+import SearchBar from './components/SearchBar/SearchBar';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div class="App">
+        <h1>ravenous</h1>
+        <SearchBar />
+        <BusinessList /> ''
+      </div>
+    );
+  }
+}
+
+export default App;
